4f6d3a7fe46210f16e5f6388e96fcfa83648a4d3
d4eh0 <eogud3332@naver.com>
2025-02-26 22:26:51 +0900

    Day3,4
    fresh사이트 마무리

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,20 +1,30 @@
 import Hello from './data.js'
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta']
+  // 부모에 하는 이유 : 자식에 하면 실행이 많아지므로.
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem/>
-      <CartItem/>
-      <Hello/>
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]}/>
+      <CartBtn color="green" />
     </div>
   )
 } 
 
-function CartItem(){ // 컴포넌트 만들기
+function CartBtn(props) {
+  return (
+    <div>
+      <button style={{ background :props.color }}>o</button>
+    </div>
+  )
+}
+
+function CartItem(props) { // 컴포넌트 만들기
   return (
   <div className="cart-item">
-    <p>상품</p>
+    <p>{props.item}</p>
     <p>$40</p>
     <p>1개</p>
   </div>
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,14 +1,27 @@
+'use client'
+import { useState } from "react"
+
 export default function List() {
   let item = ["Tomatoes", "Pasta", "Coconut"];
+  let [수량, 수량변경] = useState([0, 0, 0])
   return (
     <div>
       <h3 className="title">상품목록</h3>
       {
        item.map((a, i) => { // map함수를 이용한 html 반복
         return (
-          <div className="food" key={i}> // key추가 권장
+          <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
+            <button onClick={()=> { 
+              let copy = [...수량]
+              copy[i]--
+              수량변경(copy) }}>-</button>
+            <span> {수량[i]} </span>
+            <button onClick={()=> { 
+              let copy = [...수량]
+              copy[i]++
+              수량변경(copy) }}>+</button>
           </div>
         )
        })
